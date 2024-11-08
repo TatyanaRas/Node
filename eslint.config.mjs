69593ec35575825eb141cde0cf2cf5d66ed121b0
Tatyana Rasskazova <tatyanalipch@yandex.ru>
2024-11-08 13:48:16 +0300

    Третий коммит

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,53 +1,54 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import globals from "globals";
+import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import someConfig from "some-other-config-you-use";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-
- //@type {import('eslint').Linter.Config[]} 
 
 export default [
   { files: ['**/*.{js,mjs,cjs,jsx}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  
-  {
-    root: true,
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-    },
-    //"extends": ["eslint:recommended", "prettier"],
+  someConfig,
+  eslintConfigPrettier,
 
-    extends: ["standard", "plugin:prettier/recommended"],
-
-    parserOptions: {
-      "ecmaVersion": "latest",
-      "sourceType": "module",
-    },
-
-    plugins: ['prettier'],
-
-    rules: {
-      "no-debugger": "error",
-      "no-extra-semi": "error",
-      "no-var": "error",
-      "semi": "error",
-      "prefer-const": "error",
-      "indent": "error",
-      "no-multi-spaces": "error",
-      "space-in-parens": "error",
-      "no-multiple-empty-lines": "error",
-      "prefer-const": "error",
-      "no-use-before-define": "error",
-      "prettier/prettier": "error",
-      "arrow-body-style": "off",
-      "prefer-arrow-callback": "off",
-      "no-extra-boolean-cast": "error",
-      "no-mixed-spaces-and-tabs": "error", 
-      "no-regex-spaces": "error",
-      "no-trailing-spaces": "error",
-    },
+{
+  "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
   },
-];
+  "extends": "eslint:recommended",
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+  "rules": {
+      "arrow-parens": ["error", "as-needed"],
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "never"],
+      "quote-props": ["error", "as-needed"],
+      "semi": ["error", "always"], // точка с запятой в конце оператора
+      "quotes": ["error", "single"],
+      "linebreak-style": ["error", "unix"],
+      "indent": ["error", 4, { "SwitchCase": 1 }],
+      "no-trailing-spaces": "error",
+      "no-tabs": "error",
+      "comma-dangle": ["error", {
+          "arrays": "always-multiline",
+          "objects": "always-multiline",
+          "imports": "never",
+          "exports": "never",
+          "functions": "never"
+      }],
+      "brace-style": ["error", "1tbs"],
+      "keyword-spacing": "error",
+      "no-multi-spaces": "off",
+      "eqeqeq": "error",
+      "camelcase": "error",
+      "max-len": ["error", 100],
+      "no-multiple-empty-lines": "error"
+  }
+}  
+]
